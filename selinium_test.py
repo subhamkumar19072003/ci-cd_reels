c43e44a10029187b47e0a3ce20dc2319af55863c
subhamkumar19072003 <129536463+subhamkumar19072003@users.noreply.github.com>
2024-04-03 14:26:41 +0530

    Update selinium_test.py

--- selinium_test.py
+++ selinium_test.py
@@ -8,7 +8,7 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(service=service, options=options)
-driver.get("https://subhamkumar19072003.github.io/ci-cd_reels/")  # Update this with the path to your HTML file
+driver.get("https://github.com/subhamkumar19072003/ci-cd_reels")  # Update this with the path to your HTML file
 
 time.sleep(2)  # Adding a delay to see the result
 
